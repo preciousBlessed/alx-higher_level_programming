caf3195316649f2c6b5e2867b18e964199514422
preciousBlessed <tobechukwublessed30@yahoo.com>
2024-03-14 16:47:04 -0700

    0-Import Simple, test2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,4 +2,5 @@
 from add_0 import add
 a = 1
 b = 2
-print("<a {}> + <b {}> = <add(a, b) {}>".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
