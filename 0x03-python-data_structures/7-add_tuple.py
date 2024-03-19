9562c76a43e834269ea7fb5c88d15c9794e2e7f5
preciousBlessed <tobechukwublessed30@yahoo.com>
2024-03-18 18:50:23 -0700

    7. Tuples Addition, test4

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0 and len(tuple_b) == 0:
-        return 0, 0
-    if len(tuple_a) >=2 and len(tuple_b) == 0:
-        return tuple_a[0], tuple_a[1]
-    if len(tuple_a) == 0 and len(tuple_b) >=2:
-        return tuple_b[0], tuple_b[1]
-    if len(tuple_a) == 2 and len(tuple_b) >= 2:
-        return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    if len(tuple_a) < 2 and len(tuple_b) < 2:
-        return tuple_a[0] + tuple_b[0], 0
-    if len(tuple_a) < 2 and len(tuple_b) >= 2:
-        return tuple_a[0] + tuple_b[0], 0 + tuple_b[1]
-    if len(tuple_a) > 2 and len(tuple_b) >= 2:
-        return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    if len(tuple_a) > 2 and len(tuple_b) < 2:
-        return tuple_a[0] + tuple_b[0], tuple_a[1] + 0
-    if len(tuple_a) == 2 and len(tuple_b) < 2:
-        return tuple_a[0] + tuple_b[0], tuple_a[1] + 0
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+
+    if len(list_a) < 2:
+        for i in range(len(list_a), 2):
+            list_a.append(0)
+
+    if len(list_b) < 2:
+        for i in range(len(list_b), 2):
+            list_b.append(0)
+
+    tuple_a, tuple_b = tuple(list_a), tuple(list_b)
+    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (tuple_c)
