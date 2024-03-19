20d13534b30f6eb2cbda2344c8a12e9edae8c8a6
preciousBlessed <tobechukwublessed30@yahoo.com>
2024-03-18 17:48:16 -0700

    5. Can you C me now\?, test0

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for s in my_string:
+        if s == "c" or s == "C":
+            continue
+        new_string += s
+    return new_string
