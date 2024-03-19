94a404253595b66832e2e6262300754ca32424e7
preciousBlessed <tobechukwublessed30@yahoo.com>
2024-03-18 17:35:32 -0700

    6. List of lists=matrix, test2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1 and len(matrix[0]) == 0:
-        print()
-    else:
+    if len(matrix) > 1 and len(matrix[0]) != 0:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if j < len(matrix[i]) - 1:
-                    print("{}".format(matrix[i][j]), end = " ")
+                    print("{:d}".format(matrix[i][j]), end = " ")
                 else:
-                    print("{}".format(matrix[i][j]))
+                    print("{:d}".format(matrix[i][j]))
+    else:
+        print()
